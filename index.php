72fc0fe2e1f590f62d189e76124de6eee3b7b7c6
JasonMax_2 <jason-max.vark@khk.ee>
2014-09-29 15:37:37 +0300

    final commit

--- index.php
+++ index.php
@@ -5,7 +5,35 @@
     <meta http-equiv="Content-Type" content="text/html;
     charset=utf-8">
    </head>
-   <body>
+<body>
+<h2>Funktsioon "continue"</h2>
+   	<?php
+    for ($number = 1; $number <= 10; $number++) {
+    if ($number == 5) {
+       	continue;
+    }
 
-   </body>
+    echo $number . ", ";
+    			}
+			?>
+
+<?php
+//while ($number = 1; $number <= 10; $number++) {
+//if ($number == 5) {
+//continue;
+//}
+//echo $number . ", ";
+//}
+?>
+<h2>Funktsioon "break"</h2>
+<?php
+for ($number = 1; $number <= 10; $number++) {
+if ($number == 6) {
+    break;
+    				}
+echo $number . ", ";
+    			}
+			?>
+
+ </body>
 </html>

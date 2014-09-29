9e6f006cb8378f12cd87ae8bc3a30ca70189c889
JasonMax_2 <jason-max.vark@khk.ee>
2014-09-29 15:28:41 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+   <head>
+    <title>Harjutus-8</title>
+    <meta http-equiv="Content-Type" content="text/html;
+    charset=utf-8">
+   </head>
+   <body>
+
+   </body>
+</html>
